db23abb558dc5d8ef7a87a89c9b31f2f7d63ff82
Tito Miguel Costa <titomiguelcosta@gmail.com>
2020-11-27 17:43:29 +0100

    fix api url

--- web/src/Rate.js
+++ web/src/Rate.js
@@ -5,47 +5,47 @@ import { Currency } from './Currency';
 export class Rate extends React.Component {
   constructor(props) {
     super(props);
-    this.url ='https://pocket.titomiguelcosta.com'
+    this.url = 'https://api.pocket.titomiguelcosta.com'
     this.state = {
-        description: null,
-        source: 'EUR',
-        target: 'NZD'
+      description: null,
+      source: 'EUR',
+      target: 'NZD'
     }
   }
 
   handleSubmit(e) {
     e.preventDefault()
-    this.setState({description: null})
+    this.setState({ description: null })
     fetch(`${this.url}/rate/${this.state.source}/${this.state.target}`)
-        .then((res) => res.json())
-        .then((data) => {
-            this.setState({description: data.description})
-        })
-        .catch((error) => {
-            this.setState({description: "Failed to connect."})
-        })
+      .then((res) => res.json())
+      .then((data) => {
+        this.setState({ description: data.description })
+      })
+      .catch((error) => {
+        this.setState({ description: "Failed to connect." })
+      })
   }
 
   handleCurrency(e, widget) {
-    this.setState({[widget]: e.target.value})
+    this.setState({ [widget]: e.target.value })
   }
 
   render() {
     return (
       <div className="form">
         <Form onSubmit={(e) => this.handleSubmit(e)}>
-            <Currency name="Source" field="source"  default={this.state.source} handleCurrency={this.handleCurrency.bind(this)} />
-            <Currency name="Target" field="target" default={this.state.target} handleCurrency={this.handleCurrency.bind(this)} />
+          <Currency name="Source" field="source" default={this.state.source} handleCurrency={this.handleCurrency.bind(this)} />
+          <Currency name="Target" field="target" default={this.state.target} handleCurrency={this.handleCurrency.bind(this)} />
 
-            <FormGroup>
-                <Button>Calculate</Button>
-            </FormGroup>
+          <FormGroup>
+            <Button>Calculate</Button>
+          </FormGroup>
         </Form>
         {
-            this.state.description &&
-            <Alert color="dark" id="output">
-                {this.state.description}
-            </Alert>
+          this.state.description &&
+          <Alert color="dark" id="output">
+            {this.state.description}
+          </Alert>
         }
       </div>
     )
